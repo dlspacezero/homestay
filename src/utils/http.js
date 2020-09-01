daf58cd9fd403ae9103cfd301d7b5600b7192783
wuhu <647240979@qq.com>
2020-09-01 14:40:40 +0800

    a little change

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { reject } from "core-js/fn/promise";
+
 //公共配置
 const instance = axios.create({
     baseURL: '',
     timeout: 10000,
     headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
 });
+
 //响应拦截器
 axios.interceptors.response.use(function (response) {
     // 对响应数据做点什么
@@ -14,6 +16,7 @@ axios.interceptors.response.use(function (response) {
     // 对响应错误做点什么
     return Promise.reject(error);
 });
+
 export const http = {
     get(url,params){
         return new Promise((resolve,reject)=>{
