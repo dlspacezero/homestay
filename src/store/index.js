2349b0c2c0d654941e2e8467bf5c60b34f6e6403
wuhu <647240979@qq.com>
2020-09-04 15:53:18 +0800

    增加请求数据的格式

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,25 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
+//导入获取数据的方法
+import { getBannerList } from "../utils/api";
 export default new Vuex.Store({
     state: {
         clChooseCity: '全部城市',
         clChooseSort: '默认样式',
+        //轮播图图片数组
+        bannerlist:[]
     },
     mutations: {
+        //更新state属性
+        updateState(state,payload){
+            //payload===>{ 'key' : "value"} key对应state里属性名
+            //拿到payload键的数组，遍历数组
+            Object.keys(payload).forEach((key)=>{
+                //给state里与key同名的属性赋值
+                state[key] = payload[key];
+            });
+        },
         // 收藏页获取城市名
         getClChooseCity(state, payload) {
             state.clChooseCity = payload.city;
@@ -18,6 +30,16 @@ export default new Vuex.Store({
             state.clChooseSort = payload.sort;
         },
     },
-    actions: {},
+    actions: {
+        async changeBannerList({ commit },payload){
+            //调用api方法发送请求获取数据
+            const response = await getBannerList();
+            // console.log(response.data.list);
+            //commit 触发updateState更新属性
+            commit('updateState',{
+                'bannerlist' : response.data.list
+            });
+        }
+    },
     modules: {}
 })
