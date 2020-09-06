004838a87eddcd963ea93df3c657d54966274b7b
saudade-cxg <cxg437022665@163.com>
2020-09-06 22:09:28 +0800

    发现页数据请求更新+订单页进行页面和完成页面

--- src/components/do/do-main.vue
+++ src/components/do/do-main.vue
@@ -19,165 +19,43 @@
       ref="changebanner"
     >
       <!-- 推荐 -->
-      <van-swipe-item class="first">
-        <div class="recommend-wrap">
-          <p class="hs-recommend-likes">猜你喜欢</p>
-          <ul>
-            <li v-for="item in 10" :key="item" @click="toRecommend(1)">
-              <div class="hs-recommend-promulator-photo">
-                <img src="../../assets/images/hs-photo-01.jpg" alt />
-              </div>
-              <p class="hs-recommend-promulator-title">住进霍格沃兹的魔法城堡！上海木屋民宿！</p>
-              <div class="hs-recommend-promulator">
-                <div class="hs-recommend-promulator-headshot">
-                  <img src="../../assets/images/touxiang.jpg" alt />
-                </div>
-                <span class="iconfont icon-dianzan">367</span>
-                <span class="iconfont icon-pinglun">2</span>
-              </div>
-            </li>
-          </ul>
-          <div class="hs-getMore van-hairline--surround">查看更多</div>
-        </div>
+      <van-swipe-item>
+        <rec />
       </van-swipe-item>
       <!-- 网红民宿 -->
-      <van-swipe-item class="second">
-        <div class="hot-wrap">
-          <ul>
-            <li v-for="item in 4" :key="item" @click="toRecommend(1)">
-              <div class="hs-hot-photo">
-                <div class="hs-hot-photo-big">
-                  <img src="../../assets/images/hs-photo-01.jpg" alt />
-                </div>
-                <div class="hs-hot-photo-small-wrap">
-                  <div class="hs-hot-photo-small">
-                    <img src="../../assets/images/hs-photo-01.jpg" alt />
-                  </div>
-                  <div class="hs-hot-photo-small">
-                    <img src="../../assets/images/hs-photo-01.jpg" alt />
-                  </div>
-                </div>
-              </div>
-              <p class="hs-hot-promulator-title">住进霍格沃兹的魔法城堡！上海木屋民宿！</p>
-              <p class="hs-hot-promulator">昔风里 小木屋</p>
-              <div class="hs-hot-likes">
-                <div>
-                  <div class="hs-hot-likes-headshot">
-                    <img src="../../assets/images/touxiang.jpg" alt />
-                  </div>
-                  <div class="hs-hot-likes-headshot headshot1">
-                    <img src="../../assets/images/touxiang.jpg" alt />
-                  </div>
-                  <div class="hs-hot-likes-headshot headshot2">
-                    <img src="../../assets/images/touxiang.jpg" alt />
-                  </div>
-                </div>
-                <span class="iconfont icon-dianzan">
-                  <span class="hs-hot-likes-num">368</span>
-                </span>
-              </div>
-            </li>
-          </ul>
-          <div class="hs-getMore van-hairline--surround">查看更多</div>
-        </div>
+      <van-swipe-item>
+        <hot />
       </van-swipe-item>
       <!-- 体验分享 -->
-      <van-swipe-item  class="third">
-        <!-- 主体内容部分 -->
-        <section class="hs-content">
-          <div class="hs-houseInfo" v-for="(item,index) in 6" :key="index" @click="toRecommend(1)">
-            <div class="hs-houseImg">
-              <img src="../../assets/images/hs-photo-01.jpg" alt />
-              <span class="hf-position">杭州</span>
-            </div>
-            <div class="hs-houseDescribe">第一次来杭州，住西湖边清新loft</div>
-            <div class="hs-houseDiscuss">
-              <span class="hs-headImg">
-                <img src="../../assets/images/touxiang.jpg" alt />
-              </span>
-              <span class="iconfont icon-dianzan hs-goodNum">144</span>
-              <span class="iconfont icon-pinglun hs-discuss">2</span>
-            </div>
-          </div>
-          <div class="hs-getMore van-hairline--surround">查看更多</div>
-        </section>
+      <van-swipe-item>
+        <exp />
       </van-swipe-item>
       <!-- 特色房源 -->
-      <van-swipe-item  class="fourth">
-        <section class="hs-featureHouse">
-          <div
-            class="hs-typeHouse"
-            v-for="(item,index) in specialList"
-            :key="index"
-            @click="toSpecial(1)"
-          >
-            <div class="hs-typeImg">
-              <img src="../../assets/images/discover-special-01.jpg" alt />
-            </div>
-            <span class="hs-typeEn">{{item.enm}}</span>
-            <span class="hs-typeCn">{{item.cnm}}</span>
-          </div>
-        </section>
+      <van-swipe-item>
+        <spe />
       </van-swipe-item>
     </van-swipe>
   </div>
 </template>
 
 <script>
+import rec from "./do-rec";
+import hot from "./do-hot";
+import exp from "./do-exp";
+import spe from "./do-spe";
 export default {
   data() {
     return {
       tabswitch: ["推荐", "网红民宿", "体验分享", "特色房源"],
       isActive: 0,
       isLoading: false,
-      specialList: [
-        {
-          img: "../../assets/images/discover-special-01.jpg",
-          enm: "Villa",
-          cnm: "别墅",
-        },
-        {
-          img: "../../assets/images/discover-special-02.jpg",
-          enm: "Sea view room",
-          cnm: "海景房",
-        },
-        {
-          img: "../../assets/images/discover-special-03.jpg",
-          enm: "Thematic features",
-          cnm: "主题特色",
-        },
-        {
-          img: "../../assets/images/discover-special-04.jpg",
-          enm: "Lovers room",
-          cnm: "情侣房",
-        },
-        {
-          img: "../../assets/images/discover-special-05.jpg",
-          enm: "loft",
-          cnm: "复式",
-        },
-        {
-          img: "../../assets/images/discover-special-06.jpg",
-          enm: "Courtyard",
-          cnm: "四合院",
-        },
-        {
-          img: "../../assets/images/discover-special-07.jpg",
-          enm: "Wooden house",
-          cnm: "木屋",
-        },
-        {
-          img: "../../assets/images/discover-special-08.jpg",
-          enm: "Ancient town",
-          cnm: "古镇客栈",
-        },
-        {
-          img: "../../assets/images/discover-special-09.jpg",
-          enm: "Famhouse",
-          cnm: "农家院",
-        },
-      ],
     };
+  },
+  components: {
+    rec,
+    hot,
+    exp,
+    spe,
   },
   methods: {
     // 点击tab滑动下面的轮播
@@ -188,14 +66,6 @@ export default {
     // 滑动下面的轮播上面的tab切换
     onChange(i) {
       this.isActive = i;
-    },
-    // 进入推荐、网红民宿、体验分享详情页
-    toRecommend(id) {
-      this.$router.push("/detail/recommend/" + id);
-    },
-    // 进入特色房源详情页
-    toSpecial(id) {
-      this.$router.push("/detail/special/" + id);
     },
   },
 };
@@ -241,286 +111,6 @@ export default {
     img {
       width: 100%;
       height: 100%;
-    }
-    .van-swipe-item .recommend-wrap {
-      display: flex;
-      flex-direction: column;
-      padding: 0 15px;
-      .hs-recommend-likes {
-        margin: 4px 0 0 0;
-        width: 100%;
-        font-size: 18px;
-        @include pingfang;
-        font-weight: Bold;
-        color: #333333;
-      }
-      ul {
-        width: 100%;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        li {
-          width: 164px;
-          margin: 15px 0;
-          .hs-recommend-promulator-photo {
-            width: 164px;
-            height: 117px;
-            background: skyblue;
-            border-radius: 10px;
-            overflow: hidden;
-          }
-          .hs-recommend-promulator-title {
-            width: 164px;
-            font-size: 14px;
-            color: #656565;
-            margin: 16px 0 14px 0;
-          }
-          .hs-recommend-promulator {
-            display: flex;
-            align-items: center;
-            color: #333333;
-            font-size: 11px;
-            .hs-recommend-promulator-headshot {
-              width: 20px;
-              height: 20px;
-              background: red;
-              border-radius: 50%;
-              overflow: hidden;
-            }
-          }
-        }
-      }
-      .hs-getMore {
-        margin-top: 28px;
-        width: 345px;
-        height: 55px;
-        line-height: 55px;
-        text-align: center;
-        color: $themecolor;
-        margin-bottom: 43px;
-        font-size: 15px;
-        font-weight: bold;
-      }
-      .van-hairline--surround::after {
-        border-color: $themecolor;
-      }
-    }
-    .van-swipe-item .hot-wrap {
-      display: flex;
-      flex-direction: column;
-      padding: 0 15px;
-      ul {
-        width: 100%;
-        li {
-          width: 100%;
-          margin-top: 26px;
-          display: flex;
-          flex-direction: column;
-          border-bottom: 1px solid #e1e1e1;
-          &:last-child {
-            border: none;
-          }
-          .hs-hot-photo {
-            display: flex;
-            justify-content: space-between;
-            .hs-hot-photo-big {
-              width: 208px;
-              height: 197px;
-              background: skyblue;
-              border-radius: 10px;
-              overflow: hidden;
-            }
-            .hs-hot-photo-small-wrap {
-              display: flex;
-              flex-direction: column;
-              justify-content: space-between;
-              .hs-hot-photo-small {
-                width: 129px;
-                height: 94px;
-                background: skyblue;
-                border-radius: 10px;
-                overflow: hidden;
-              }
-            }
-          }
-          .hs-hot-promulator-title {
-            margin: 15px 0 0 0;
-            color: #656565;
-            font-size: 14px;
-          }
-          .hs-hot-promulator {
-            margin: 0;
-            color: #656565;
-            font-size: 14px;
-          }
-          .hs-hot-likes {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            margin: 28px 0;
-            .hs-hot-likes-headshot {
-              width: 28px;
-              height: 28px;
-              background: red;
-              border-radius: 50%;
-              overflow: hidden;
-              float: left;
-            }
-            .headshot1 {
-              position: relative;
-              left: -10px;
-              z-index: -1;
-            }
-            .headshot2 {
-              position: relative;
-              left: -20px;
-              z-index: -2;
-            }
-            .hs-hot-likes-num {
-              color: #333333;
-              font-size: 11px;
-              @include pingfang;
-              font-weight: bold;
-            }
-            .iconfont {
-              color: #fe8e47;
-            }
-          }
-        }
-      }
-      .hs-getMore {
-        margin-top: 12px;
-        width: 345px;
-        height: 55px;
-        line-height: 55px;
-        text-align: center;
-        color: $themecolor;
-        margin-bottom: 43px;
-        font-size: 15px;
-        font-weight: bold;
-      }
-      .van-hairline--surround::after {
-        border-color: $themecolor;
-      }
-    }
-    .van-swipe-item {
-      .hs-content {
-        padding: 26px 15px 0px 15px;
-        display: flex;
-        justify-content: space-between;
-        flex-wrap: wrap;
-        .hs-houseInfo {
-          display: flex;
-          flex-direction: column;
-          @include pingfang;
-          margin-bottom: 38.33px;
-          .hs-houseImg {
-            width: 157px;
-            height: 149px;
-            border-radius: 10px;
-            background: crimson;
-            position: relative;
-            overflow: hidden;
-            img {
-              width: 100%;
-              height: 100%;
-            }
-            .hf-position {
-              display: inline-block;
-              width: 38px;
-              height: 20px;
-              background: #ffffff;
-              border-radius: 3px;
-              position: absolute;
-              left: 4px;
-              bottom: 4px;
-              line-height: 20px;
-              text-align: center;
-              color: #666;
-              font-weight: 500;
-              font-size: 11px;
-            }
-          }
-          .hs-houseDescribe {
-            width: 153px;
-            color: #333;
-            font-size: 14px;
-            margin-top: 10px;
-            margin-bottom: 12px;
-            font-weight: 500;
-          }
-          .hs-houseDiscuss {
-            height: 28px;
-            width: 153px;
-            line-height: 28px;
-            color: #333;
-            font-size: 11px;
-            font-weight: 500;
-            display: flex;
-            span {
-              display: inline-block;
-            }
-            .hs-headImg {
-              width: 28px;
-              height: 28px;
-              border-radius: 50%;
-              background: chocolate;
-              overflow: hidden;
-              img {
-                width: 100%;
-                height: 100%;
-              }
-            }
-          }
-        }
-        .hs-getMore {
-          width: 345px;
-          height: 55px;
-          line-height: 55px;
-          text-align: center;
-          color: $themecolor;
-          margin-bottom: 43px;
-          font-size: 15px;
-          font-weight: bold;
-        }
-        .van-hairline--surround::after {
-          border-color: $themecolor;
-        }
-      }
-      .hs-featureHouse {
-        padding: 26px 15px 0px 15px;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        .hs-typeHouse {
-          display: flex;
-          flex-direction: column;
-          color: #333;
-          @include pingfang;
-          margin-bottom: 32px;
-          .hs-typeImg {
-            width: 103px;
-            height: 116px;
-            border-radius: 6px;
-            margin-bottom: 19px;
-            img {
-              width: 100%;
-              height: 100%;
-            }
-          }
-          .hs-typeEn {
-            margin-left: 6px;
-            font-size: 11px;
-            font-weight: 500;
-            margin-bottom: 7px;
-          }
-          .hs-typeCn {
-            margin-left: 6px;
-            font-size: 14px;
-            font-weight: bold;
-          }
-        }
-      }
     }
   }
 }
---
+++ src/components/do/do-scontent.vue
@@ -0,0 +1,236 @@
+<template>
+  <!-- 特色房源详情页-内容 -->
+  <ul class="scontent-wrap">
+    <li v-for="(item,index) in 3" :key="index" @click="toDetail(1)">
+      <!-- 房型 轮播图 -->
+      <van-swipe :show-indicators="false">
+        <van-swipe-item v-for="(item,index) in 4" :key="index" class="banner">
+          <div class="banner">
+            <img src="../../assets/images/hs-photo-01.jpg" alt />
+          </div>
+        </van-swipe-item>
+      </van-swipe>
+      <!-- 房型 详细信息 -->
+      <div class="infoWrap">
+        <!-- 房东 头像 -->
+        <img src="../../assets/images/touxiang.jpg" alt="photo" class="headshot" />
+        <!-- 房型 评价 -->
+        <div class="praise">
+          <span class="iconfont icon-dianzan"></span>
+          <span>5.0</span>
+          <span style="color:#FD8735">"超赞"</span>
+          <span>2条评价</span>
+        </div>
+        <!-- 房型 标题 -->
+        <div class="title">
+          <span class="cyber">网红民宿</span>
+          <span class="h2">卓小花【浪漫满屋】步行人民公园/天府广场春熙路/骑行文殊院</span>
+        </div>
+        <!-- 房型 信息 -->
+        <div class="info">
+          <span>整租-2居室-宜住6人</span>
+          <span class="address">成都锦江区</span>
+        </div>
+        <!-- 房型 亮点 -->
+        <div class="ableDo">
+          <span>可做饭</span>
+        </div>
+        <!-- 房型 价格 -->
+        <div class="price">
+          <span class="unit">¥</span>
+          <span class="num">338</span>
+          <span class="evn">/晚</span>
+        </div>
+      </div>
+      <!-- 房型 收藏 -->
+      <div class="likes">
+        <img :src="unlikeimg" alt="pic" class="like" v-if="like==0" @click.stop="likes" />
+        <img :src="likeimg" alt="pic" class="like" v-if="like==1" @click.stop="likes" />
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+import likeimg from "@/assets/imgs/icon_sc.png";
+import unlikeimg from "@/assets/imgs/icon_sc2.png";
+import { Toast } from "vant";
+export default {
+  data() {
+    return {
+      like: 0,
+      unlikeimg: unlikeimg,
+      likeimg: likeimg,
+    };
+  },
+  methods: {
+    // 头部照片返回上一级路由
+    toBack() {
+      this.$router.go(-1);
+    },
+    // 收藏、取消收藏：
+    likes() {
+      if (this.like == 0) {
+        Toast({
+          message: "收藏成功！",
+          position: "bottom",
+        });
+        this.like++;
+      } else if (this.like == 1) {
+        Toast({
+          message: "取消收藏成功！",
+          position: "bottom",
+        });
+        this.like--;
+      }
+    },
+    // 点击列表页进 主详情页的house
+    toDetail(id) {
+      this.$router.push("/detail/house/" + id);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.scontent-wrap {
+  background: #f0f0f0;
+  li {
+    height: 360px;
+    background: #ffffff;
+    border-radius: 5px;
+    margin: 0 15px;
+    padding: 8px 8px 0;
+    margin-bottom: 16px;
+    position: relative;
+    .banner {
+      width: 330px;
+      height: 212px;
+      border-radius: 10px;
+      img {
+        width: 100%;
+      }
+    }
+    .infoWrap {
+      margin: 0 8px;
+      position: relative;
+      .headshot {
+        width: 47px;
+        height: 47px;
+        border: 2px solid #ffffff;
+        border-radius: 50%;
+        position: absolute;
+        top: -15px;
+        right: 0px;
+      }
+      .praise {
+        margin: -13px 0 10px 0;
+        width: 130px;
+        height: 30px;
+        background: #ffffff;
+        box-shadow: 0px 2px 8px 0px rgba(104, 102, 102, 0.13);
+        border-radius: 15px;
+        line-height: 30px;
+        font-size: 11px;
+        font-family: PingFang SC;
+        font-weight: bold;
+        color: #999999;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        .icon-dianzan {
+          color: #fd8735;
+        }
+      }
+      .title {
+        height: 31px;
+        display: flex;
+        align-items: center;
+        .cyber {
+          width: 49px;
+          height: 16px;
+          line-height: 16px;
+          background: linear-gradient(89deg, #a33def 0%, #1383fd 100%);
+          border-radius: 2px;
+          text-align: center;
+          font-size: 11px;
+          font-family: "PingFang SC";
+          font-weight: bold;
+          color: #f7f7f7;
+        }
+        .h2 {
+          width: 210px;
+          height: 31px;
+          margin-left: 13px;
+          line-height: 31px;
+          font-size: 15px;
+          font-family: "PingFang SC";
+          font-weight: bold;
+          color: #333333;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+      }
+      .info {
+        height: 24px;
+        line-height: 24px;
+        font-size: 11px;
+        font-family: "PingFang SC";
+        font-weight: 500;
+        color: #656565;
+        span {
+          margin-right: 11px;
+        }
+      }
+      .ableDo {
+        height: 17px;
+        padding-bottom: 10px;
+        display: flex;
+        border-bottom: 1px solid #e1e1e1;
+        span {
+          height: 17px;
+          margin-right: 4px;
+          border: 1px solid #e1e1e1;
+          border-radius: 2px;
+          line-height: 17px;
+          font-size: 11px;
+          font-family: PingFang SC;
+          font-weight: 500;
+          color: #656565;
+        }
+      }
+      .price {
+        height: 48px;
+        line-height: 48px;
+        font-family: PingFang SC;
+        span.unit {
+          font-size: 14px;
+          font-weight: bold;
+          color: #333333;
+        }
+        span.num {
+          font-size: 20px;
+          font-weight: bold;
+          color: #333333;
+        }
+        span.evn {
+          font-size: 14px;
+          font-weight: 500;
+          color: #333333;
+        }
+      }
+    }
+    .likes {
+      position: absolute;
+      top: 24px;
+      right: 26px;
+      width: 19px;
+      height: 17px;
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
